a814f563fd49846fb4ae1a9df8cd3850060f5435
liebeeeee22 <dhanzganteng6@gmail.com>
2026-05-24 17:12:46 +0700

    Create MainActivity.java

---
+++ app/src/main/app/src/main/java/com/aegis/assistant/MainActivity.java
@@ -0,0 +1,60 @@
+package com.aegis.assistant;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int OVERLAY_PERMISSION_REQ = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(v -> checkOverlayPermission());
+    }
+
+    private void checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ);
+            } else {
+                startOverlayService();
+            }
+        } else {
+            startOverlayService();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OVERLAY_PERMISSION_REQ) {
+            if (Settings.canDrawOverlays(this)) {
+                startOverlayService();
+            } else {
+                Toast.makeText(this, "Izin overlay diperlukan!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startOverlayService() {
+        Intent intent = new Intent(this, OverlayService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent);
+        } else {
+            startService(intent);
+        }
+        Toast.makeText(this, "Visual Assist Aktif!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+                                       }
